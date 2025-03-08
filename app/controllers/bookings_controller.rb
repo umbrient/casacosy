3e870251982182e323684438d97f2d96297fea91
Max <umbrient@gmail.com>
2025-03-08 13:46:07 +0000

    add contingency

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,8 +4,8 @@ class BookingsController < ApplicationController
   
   def show 
     @booking = Booking.find(params[:id])&.decorate
-    @img = aws_api.get_id_link(request: @booking.requests.id.last)
-    @terms = aws_api.get_id_link(request: @booking.requests.terms.last)
+    @img = aws_api.get_id_link(request: @booking.requests.id.last) if @booking&.requests&.id&.last
+    @terms = aws_api.get_id_link(request: @booking.requests.terms.last) if @booking&.requests&.terms&.last
     redirect_to(root_path) unless @booking
   end 
 
