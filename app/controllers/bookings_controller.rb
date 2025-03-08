da7b59ce3deb6a0dfb9b498fba1dc1b3986b4069
Max <umbrient@gmail.com>
2025-03-08 13:42:46 +0000

    add ID and terms preview

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,7 +4,16 @@ class BookingsController < ApplicationController
   
   def show 
     @booking = Booking.find(params[:id])&.decorate
+    @img = aws_api.get_id_link(request: @booking.requests.id.last)
+    @terms = aws_api.get_id_link(request: @booking.requests.terms.last)
     redirect_to(root_path) unless @booking
   end 
+
+
+  private 
+
+  def aws_api 
+    @aws_api ||= AwsApi.new
+  end
   
 end
